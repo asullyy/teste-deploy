6b19326b307c1f16daf6f213dc752d61dba4e8f9
Karen Silva <sullianyy@gmail.com>
2024-02-21 13:44:38 -0300

    deploy3

---
+++ src/pages/Menu.jsx
@@ -0,0 +1,13 @@
+import "./Menu.css"
+
+function Menu(){
+    return(
+        <div>
+            <a href="/">Home</a>
+            <a href="/teste-deploy/About">About</a>
+            <a href="/teste-deploy/Contact">Contact</a>
+        </div>
+    )
+}
+
+export default Menu
